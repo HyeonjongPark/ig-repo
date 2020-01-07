fce0f5165413137ade801d190409f69ac9306cfb
HyeonjongPark <guswhd9844@gmail.com>
2020-01-07 17:30:45 +0900

    data_merge

---
+++ data_merge_linux.R
@@ -0,0 +1,23 @@
+library(DataExplorer)
+library(data.table)
+library(dplyr)
+
+aud = fread("/root/data/audience_profile_pre.csv")
+aud = aud[,-c(1,6,7,9)]
+aud[aud == ""] = NA
+
+fwrite(aud,"/root/data/aud_ver1.csv")
+
+train = fread("/root/data/train.csv")
+test = fread("/root/data/test.csv")
+
+
+join_train_ver1 = left_join(train, aud_ver1, by = c("device_ifa" = "device_ifa"))
+join_train_ver1[join_train_ver1 == ""] = NA
+fwrite(join1, "/root/data/join_train_ver3_2.csv")
+
+join_test_ver1 = left_join(test, aud_ver1, by = c("device_ifa" = "device_ifa"))
+join_test_ver1[join_test_ver1 == ""] = NA
+fwrite(join_test_ver1, "join_test_ver3_2.csv")
+
+
